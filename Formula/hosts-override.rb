38b883e6b236c740c07512af79baaa3ac88b7cdb
Ryan Caught <rcaught@gmail.com>
2020-10-20 13:59:10 +1000

    Update hosts-override.rb

--- Formula/hosts-override.rb
+++ Formula/hosts-override.rb
@@ -2,8 +2,8 @@ class HostsOverride < Formula
   desc "Override hosts file entries for the lifetime of the process"
   homepage "https://github.com/rcaught/hosts-override"
   url "https://github.com/rcaught/hosts-override.git",
-      :tag      => "0.5.0",
-      :revision => "02b63e305fdd956d0aa7b71af6b8def9991b45eb"
+      :tag      => "0.6.0",
+      :revision => "e931fdbaded6d88fcc91af89de33a512dd76b56f"
   head "https://github.com/rcaught/hosts-override.git"
 
   depends_on "go" => :build
